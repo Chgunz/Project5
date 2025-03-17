b52cd116f1bb4b87bc703edf980bdf4896ccbbf0
Chgunz <andyboy420019@gmail.com>
2025-03-16 21:43:07 -0700

    Created the Trvia Game

---
+++ TriviaGame/TriviaGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TriviaGame
+//
+//  Created by Andy Hernandez on 3/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TriviaGame/TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Andy Hernandez on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
